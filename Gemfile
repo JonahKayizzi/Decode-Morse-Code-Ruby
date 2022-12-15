cf5eb5ecf9a73bf946eba37b8d0ea648e02f70aa
Miliyonayalew <miliayalew@gmail.com>
2022-12-15 03:11:54 +0300

    Fix rubocop error

--- Gemfile
+++ Gemfile
@@ -1,8 +1,4 @@
-# frozen_string_literal: true
-
-source "https://rubygems.org"
-
-git_source(:github) { |Decode-Morse-Code-Ruby| "https://github.com/#{Decode-Morse-Code-Ruby}" }
+source 'https://rubygems.org'
 
 # gem "rails"
 gem 'rubocop', '>= 1.0', '< 2.0'
--- morse.rb
+++ morse.rb
@@ -1,46 +1,54 @@
-def decode_char(morseChar)
-    characters = { 
-    ".-" => 'A', 
-    "-..." => 'B',
-     "-.-." => 'C',
-     "-.." => 'D',
-     "." => 'E',
-     "..-." => 'F',
-     "--." => 'G',
-     "...." => 'H',
-     ".." => 'I',
-     ".---" => 'J',
-     "-.-" => 'K',
-     ".-.." => 'L',
-     "--" => 'M',
-     "-." => 'N',
-     "---" => 'O',
-     ".--." => 'P',
-     "--.-" => 'Q',
-     ".-." => 'R',
-     "..." => 'S',
-     "-" => 'T',
-     "..-" => 'U',
-     "...-" => 'V',
-     ".--" => 'W',
-     "-..-" => 'X',
-     "-.--" => 'Y',
-     "--.." => 'Z'
-    }
-    characters[morseChar].upcase
+CHARACTERS = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def decode_char(morse_char)
+  CHARACTERS[morse_char].upcase
 end
 
 def decode_word(morse_word)
   word = ''
-  wordArray = morse_word
-  wordArray.split.each { |i| word += decode_char(i) }
+  word_array = morse_word
+  word_array.split.each { |i| word += decode_char(i) }
   word
 end
-def decode_message(morseMessage)
-  messageArray = morseMessage.split('   ')
+
+def decode_message(morse_message)
+  message_array = morse_message.split('   ')
   output = ''
-  messageArray.each { |i| output += "#{decode_word(i)} " }
+  message_array.each { |i| output += "#{decode_word(i)} " }
   output
 end
 
-
+# Test
+puts decode_char('--') # M
+puts decode_word('-- .. .-.. .-') # MILA
+puts decode_word('.--- --- -.') # JON
+puts decode_message('-- -.--   -. .- -- .') # MY NAME
+old_bottle = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+puts decode_message(old_bottle) # A BOX FULL OF RUBIES
